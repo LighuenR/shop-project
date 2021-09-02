2dc92a5be2594b189a5aeb0ddc97a0470de086ac
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-02 17:14:31 -0300

    banner and nav bar

---
+++ Frontend/src/components/Home/Banner.vue
@@ -0,0 +1,21 @@
+!<template>
+    <div class="banner-section">
+    </div>
+       
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.banner-section {
+   background: url("../../assets/banner.jpg");
+   min-height: 70vh;
+   background-size: cover;
+   background-attachment: fixed;
+   background-position: center;
+}
+</style>
