c0b8c75e65f0ae04c3047f216531c8882acbf734
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-03 12:06:14 -0300

    Header uploaded

--- Frontend/src/components/layout/Header.vue
+++ Frontend/src/components/layout/Header.vue
@@ -35,12 +35,27 @@
     </v-app-bar>
     
           <v-navigation-drawer
+      class="drawer"
       v-model="drawer"
       absolute
       app
+      dark
       right
       temporary
     >
+    <h1 class="pl-3 pt-2">Your Cart</h1>
+    <v-container>
+      <v-row>
+        <v-col class="cols-12" dark>
+          <h3>Crazy leggins</h3>
+          <h5>Amount: 2</h5>
+          <h4>Price: $80</h4>
+          <b-btn><v-icon>mdi-delete</v-icon></b-btn>
+        </v-col>
+      </v-row>
+
+      <h2>Total: $160</h2>
+      </v-container>
     
     </v-navigation-drawer>
     </header>
@@ -57,5 +72,8 @@
 </script>
 
 <style lang="scss" scoped>
+.drawer {
+  color: white;
+}
 
 </style>
