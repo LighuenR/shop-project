eb8b27eb065d28e562d68ce76b54da19188eabdf
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-08 19:57:59 -0300

    data table

--- Frontend/src/components/Cart/Cart.vue
+++ Frontend/src/components/Cart/Cart.vue
@@ -1,23 +1,48 @@
 !<template>
-    <div class="cart">
-        <div class="container">
-        <h1>Cart</h1>
-        <h1>Cart</h1>
-        <h1>Cart</h1>
-        <h1>Cart</h1>
-        </div>
-    </div>
+ <div class="container">
+  <v-data-table
+    :headers="headers"
+    :items="products"
+    :items-per-page="5"
+    class="elevation-1"
+
+  ></v-data-table>
+</div>
 </template>
 
 <script>
     export default {
-        
-    }
+         data () {
+      return {
+          headers: [
+            {
+            text: 'Products',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Price', value: 'price' },
+          { text: 'Size', value: 'size' },
+          { text: 'Description', value: 'description' },
+          { text: 'Cantidad', value: 'cantidad' },
+          { text: 'Total', value: 'total'}
+       ],
+         products: [
+          {
+            name: 'Jean loco',
+            price: "$500",
+            size: "XL",
+            description: "Una jean de la hostia que no se rompe con nada",
+            cantidad: 2,
+            total: "$1000"
+          }]
+      }
+    }}
 </script>
 
 <style lang="scss" scoped>
 .container {
-    padding-top:50px;
+    padding:75px 0 300px 0;
 }
 .cart {
   display: grid;
