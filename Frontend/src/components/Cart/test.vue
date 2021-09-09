0f21c4c09c09bc4c040575ad937c1be27e0e4cc1
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-09 10:34:23 -0300

    Img changes

---
+++ Frontend/src/components/Cart/test.vue
@@ -0,0 +1,10 @@
+
+         products: [
+          {
+            name: 'Jean loco',
+            price: "$500",
+            size: "XL",
+            description: "Una jean de la hostia que no se rompe con nada",
+            cantidad: 2,
+            total: "$1000"
+          }]
