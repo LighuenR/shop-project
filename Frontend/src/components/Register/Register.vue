fec54b0d31d0738ac020d594ab0701adf4e7715f
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-09 19:29:27 -0300

    register view ready

--- Frontend/src/components/Register/Register.vue
+++ Frontend/src/components/Register/Register.vue
@@ -1,63 +1,59 @@
 !<template>
     <div class="container">
         <v-card>
+          <v-card-title>
+           Create account
+          </v-card-title>
  <v-form
     ref="form"
-    v-model="valid"
+      class="pa-4"
+ 
     lazy-validation
   >
     <v-text-field
       v-model="name"
       :counter="10"
-      :rules="nameRules"
-      label="Name"
+      label="Enter your name"
       required
     ></v-text-field>
+
+     <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Enter your password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
 
     <v-text-field
       v-model="email"
-      :rules="emailRules"
-      label="E-mail"
+      label="Enter your email"
       required
     ></v-text-field>
 
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
+     <v-text-field
+      v-model="phone"
+      label="Enter your phone number"
       required
-    ></v-select>
+    ></v-text-field>
+   
 
     <v-checkbox
       v-model="checkbox"
       :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
+      label="Do you accept the terms and conditions?"
       required
     ></v-checkbox>
 
     <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
+      color="#275660"
+      dark
     >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
+      Register
     </v-btn>
   </v-form>
   </v-card>
@@ -66,12 +62,22 @@
 
 <script>
     export default {
-        
-    }
+        data() {
+           return {
+        show1: false,
+        password: '',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          
+        },
+        }
+    }}
 </script>
 
 <style lang="scss" scoped>
 .container {
     padding:75px 0 300px 0;
 }
+
 </style>
