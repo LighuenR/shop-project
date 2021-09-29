92395aa1a8ceba0872595d5cfc4d1c2358d82d98
LighuenRestanio <lighuen.restanio@gmail.com>
2021-09-29 10:31:49 -0300

    Space

--- Frontend/src/components/layout/Header.vue
+++ Frontend/src/components/layout/Header.vue
@@ -14,9 +14,9 @@
           </template>
 
           <v-list>
-            <v-list-item> <a href="/"> Home </a></v-list-item>
-            <v-list-item> <a href="/register"> Register </a></v-list-item>
-            <v-list-item> <a href="/login">Login </a> </v-list-item>
+            <v-list-item><a href="/"> Home </a></v-list-item>
+            <v-list-item><a href="/register"> Register </a></v-list-item>
+            <v-list-item><a href="/login">Login </a> </v-list-item>
             <v-list-item><a href="/cart"> Your cart </a></v-list-item>
             <v-list-item><a href="/admin"> Admin </a></v-list-item>
           </v-list>
@@ -28,86 +28,71 @@
       </v-container>
     </v-app-bar>
 
-    <v-navigation-drawer
-      class="drawer"
-      v-model="drawer"
-      app
-      right
-    >
-    <v-container>
-     <h2 class="pa-2">Your Cart</h2>
+    <v-navigation-drawer class="drawer" v-model="drawer" app right>
+      <v-container>
+        <h2 class="pa-2">Your Cart</h2>
 
-      <v-divider></v-divider>
-  
-      <v-card
-    class="mx-auto"
-  
-    outlined v-for="(product, i) in cartProducts"  :key="i"
-  >
+        <v-divider></v-divider>
 
- <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title class="text-h7 mb-1">
-          {{product.name}}
-        </v-list-item-title>
-        <v-list-item-subtitle>{{product.description}}</v-list-item-subtitle>
-      </v-list-item-content>
+        <v-card
+          class="mx-auto"
+          outlined
+          v-for="(product, i) in cartProducts"
+          :key="i"
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h7 mb-1">
+                {{ product.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{
+                product.description
+              }}</v-list-item-subtitle>
+            </v-list-item-content>
 
-      <v-list-item-avatar
-        tile
-        size="80"
-      
-      >
-      <v-img :src="product.img"></v-img>
-      </v-list-item-avatar>
+            <v-list-item-avatar tile size="80">
+              <v-img :src="product.img"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
 
-    </v-list-item>
+          <v-card-actions>
+            <v-btn
+              outlined
+              rounded
+              text
+              @click="$store.dispatch('REMOVE_PRODUCT_FROM_CART', product._id)"
+            >
+              Remove
+            </v-btn>
+            <h4 class="ml-4">Price: ${{ product.price }}</h4>
+          </v-card-actions>
+        </v-card>
 
-    <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-        @click="$store.dispatch('REMOVE_PRODUCT_FROM_CART', product._id)"
-      >
-        Remove
-      </v-btn>
-      <h4 class="ml-4">Price: ${{product.price}}</h4>
-    </v-card-actions>
-      
-  
-  </v-card>
-
-
-      <h3 class="pa-2">Total: ${{cartTotal}}</h3>
+        <h3 class="pa-2">Total: ${{ cartTotal }}</h3>
       </v-container>
-    
-
     </v-navigation-drawer>
   </header>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       drawer: false,
     };
   },
-  computed:{
-    ...mapGetters(["cartProducts", "cartTotal"])
+  computed: {
+    ...mapGetters(["cartProducts", "cartTotal"]),
   },
-  
 };
 </script>
 
 <style lang="scss" scoped>
 .drawer {
- /* color: white; */
+  /* color: white; */
 }
 a {
   text-decoration: none;
 }
-
 </style>
